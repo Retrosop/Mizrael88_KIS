5906944f7a8970d04f5dbef44a8560003ba32f3b
Mizrael88 <118028553+Mizrael88@users.noreply.github.com>
2023-03-03 22:04:54 +1000

    Complite 15

---
+++ RunGroopWebApp/RunGroopWebApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroopWebApp.ViewModels;
+
+public class RegisterViewModel
+{
+    [Display(Name = "Адрес электронной почты")]
+    [Required(ErrorMessage = "Требуется адрес электронной почты")]
+    public string EmailAddress { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    [Display(Name = "Подтвердите пароль")]
+    [Required(ErrorMessage = "Требуется подтвердить пароль")]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Пароль не совпадает")]
+    public string ConfirmPassword { get; set; }
+}
